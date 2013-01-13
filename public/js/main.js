5385df0f454ab97b57fff7ff1a4fecf894b27943
Samori Gorse <samorigorse@gmail.com>
2013-01-13 01:41:43 +0100

    Made ListView re-usable

---
+++ public/js/main.js
@@ -0,0 +1,37 @@
+var templates = {};
+
+function getTmpl(name) {
+  if(!templates[name]) {
+    templates[name] = swig.compile($('#' + name).html(), {templateName: name});
+  }
+  return templates[name];
+}
+
+function ListView(name, columns) {
+  this.name = name;
+  this.columns = columns;
+  this.text = null;
+}
+
+ListView.prototype.fetch = function fetch(url, cb) {
+  $.getJSON(url, function(data) {
+    var context = {
+      'pageName': this.name,
+      'columns': this.columns,
+      'lines': data
+    };
+    cb(getTmpl('list')(context));
+  }.bind(this));
+};
+
+var posts = new ListView('Posts', [
+  {'label': 'Title', 'value': 'title'},
+  {'label': 'Created at', 'value': 'createdAt', 'type': 'date'},
+  {'label': 'Published', 'value': 'published', 'type': 'bool'}
+]);
+
+var users = new ListView('Users', [
+  {'label': 'Username', 'value': 'username'},
+  {'label': 'Created at', 'value': 'createdAt', 'type': 'date'},
+  {'label': 'Is staff', 'value': 'isStaff', 'type': 'bool'}
+]);
