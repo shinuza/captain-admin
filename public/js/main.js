6a987497e5e909b2dab410fc21ce2770b2d3283e
Samori Gorse <samorigorse@gmail.com>
2013-01-13 14:51:01 +0100

    Wired views to hashchange event

--- public/js/main.js
+++ public/js/main.js
@@ -13,16 +13,38 @@ function ListView(name, columns) {
   this.text = null;
 }
 
-ListView.prototype.fetch = function fetch(url, cb) {
-  $.getJSON(url, function(data) {
-    var context = {
-      'name': this.name,
-      'columns': this.columns,
-      'lines': data
-    };
-    cb(getTmpl('list')(context));
-  }.bind(this));
+ListView.prototype ={
+  fetch: function fetch(url, cb) {
+    $.getJSON(url, function(data) {
+      var context = {
+        'name': this.name,
+        'columns': this.columns,
+        'lines': data
+      };
+      cb(getTmpl('list')(context));
+    }.bind(this));
+  }
 };
+
+function MenuView(el) {
+  this.el = $(el);
+  this.links = $('a', $('#menu'));
+}
+
+MenuView.prototype = {
+  unselectAll: function unselectAll() {
+    this.links.each(function(i, link) {
+      $(link).removeClass('active');
+    });
+  },
+
+  select: function(hash) {
+    this.unselectAll();
+    this.el.find('a[href="' + hash + '"]').addClass('active');
+  }
+};
+
+var menu = new MenuView('#menu');
 
 var posts = new ListView('posts', [
   {'label': 'Title', 'value': 'title'},
@@ -35,3 +57,45 @@ var users = new ListView('users', [
   {'label': 'Created at', 'value': 'createdAt', 'type': 'date'},
   {'label': 'Is staff', 'value': 'isStaff', 'type': 'bool'}
 ]);
+
+$(function() {
+  var content = $('#content');
+
+  function onHashChanged(e) {
+    var hash = document.location.hash;
+    var route = routes[hash];
+    menu.select(hash);
+    route && route(e);
+  }
+
+  function onResized() {
+    content.css('height', document.height);
+  }
+
+  var routes = {
+    '#dashboard': function() {
+
+    },
+
+    '#posts': function() {
+      posts.fetch('http://localhost:8080/posts', function(html) {
+        content.html(html);
+      });
+    },
+
+    '#users': function() {
+      users.fetch('http://localhost:8080/users', function(html) {
+        content.html(html);
+      });
+    },
+
+    '#settings': function() {
+
+    }
+  };
+
+  onHashChanged();
+  onResized();
+  window.onhashchange = onHashChanged;
+  window.onresize = onResized
+});
