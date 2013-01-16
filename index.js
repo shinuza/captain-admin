5d7fd8cea7cc7200b47c3ff8c08e9ef60f143e0c
Samori Gorse <samorigorse@gmail.com>
2013-01-16 17:01:23 +0100

    Hardcoded media path

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express'), app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '../captainjs-media')); //TODO: Put this in setting
+app.use(express.static('/Users/shinuza/Code/captainjs-media/')); //TODO: Put this in setting
 
 app.get('/', function(req, res){
   res.sendfile(__dirname + '/views/layout.html');
