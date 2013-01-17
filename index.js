ce0ff9c2d01f32566c9cf2da9d23ec8a63a98206
Samori Gorse <samorigorse@gmail.com>
2013-01-17 01:30:15 +0100

    Removed mounting event, new style settings

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 var express = require('express'),
     app = express();
 
-app.on('mount', function() {
-  var settings = require('captainjs-core').settings;
+var settings = require('captainjs-core').getSettings();
 
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.static(settings.get('MEDIA_ROOT')));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(settings.get('MEDIA_ROOT')));
 
-  app.get('/', function(req, res){
-    res.sendfile(__dirname + '/views/layout.html');
-  });
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/views/layout.html');
 });
 
 if(require.main === module) {
