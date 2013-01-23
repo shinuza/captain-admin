b452721b23d3acca3977160df376d02762e5e208
Samori Gorse <samorigorse@gmail.com>
2013-01-23 23:36:18 +0100

    Redirecting /admin to /admin/

--- index.js
+++ index.js
@@ -7,18 +7,21 @@ app.use(express.static(core.settings.get('MEDIA_ROOT')));
 app.use(express.cookieParser());
 app.use(core.middleware.authenticate());
 
-app.on('mount', function(parent) {
-  app.get('/', function(req, res) {
-    if(!req.session) {
-      res.redirect(req.originalUrl + 'login');
-    } else {
-      res.sendfile(__dirname + '/views/layout.html');
-    }
-  });
+app.get('/', function(req, res) {
+  var url = req.originalUrl;
+  if(!url.match(/\/$/)) {
+    return res.redirect(url + '/');
+  }
 
-  app.get('/login', function(req, res) {
-    res.sendfile(__dirname + '/views/login.html');
-  });
+  if(!req.session) {
+    res.redirect(url + 'login');
+  } else {
+    res.sendfile(__dirname + '/views/layout.html');
+  }
+});
+
+app.get('/login', function(req, res) {
+  res.sendfile(__dirname + '/views/login.html');
 });
 
 module.exports = app;
