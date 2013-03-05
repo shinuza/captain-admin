1c01f41a5902968cf8db01144eff4136d0523395
Samori Gorse <samorigorse@gmail.com>
2013-03-06 00:44:57 +0100

    Compatibility 0.8.4

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var express = require('express'),
     app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(core.settings.get('MEDIA_ROOT')));
+app.use(express.static(core.modules.settings.get('MEDIA_ROOT')));
 app.use(express.cookieParser());
-app.use(core.middleware.authenticate());
+app.use(core.modules.middleware.authenticate());
 
 app.get('/', function(req, res) {
   var url = req.originalUrl;
